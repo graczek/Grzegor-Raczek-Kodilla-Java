15e4ed288c338b963b99962e419c48263e20c377
graczek <raczek.grz@gmail.com>
2019-09-18 21:46:39 +0200

    task 7.1 cleanup

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -13,24 +13,5 @@ public class StreamMain {
         poemBeutifier.beautify("sampletext4", textToBeautify -> textToBeautify.replaceAll("sample", "lambda"));
         poemBeutifier.beautify("sampletext5", textToBeautify -> textToBeautify.substring(6));
 
-
-//        Processor processor = new Processor();
-//        processor.execute(() -> System.out.println("This is an example text with lambda."));
-
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//
-//        System.out.println("Calculating expressions with lambdas");
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-//
-//        System.out.println("Calculating expressions with menthod references");
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subAFromB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-
     }
 }
