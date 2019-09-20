a87ba438a538af3dfec22e20ceb25b78a376480a
graczek <raczek.grz@gmail.com>
2019-09-20 12:01:49 +0200

    adding 7.2 stream exercises

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeutifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -12,6 +13,9 @@ public class StreamMain {
         poemBeutifier.beautify("sampletext3", textToBeautify -> textToBeautify.concat("ABC"));
         poemBeutifier.beautify("sampletext4", textToBeautify -> textToBeautify.replaceAll("sample", "lambda"));
         poemBeutifier.beautify("sampletext5", textToBeautify -> textToBeautify.substring(6));
+
+        System.out.println("Using stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
