87c189583960d1098ba6c24babc7002069f680c2
graczek <raczek.grz@gmail.com>
2019-09-24 22:09:18 +0200

    Rock, Paper, Scissors game init - initial concept, in progress

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/Player.java
@@ -0,0 +1,13 @@
+package com.kodilla.rps;
+
+public class Player {
+    private String playersName;
+
+    public Player(String playersName) {
+        this.playersName = playersName;
+    }
+
+    public String getPlayersName() {
+        return playersName;
+    }
+}
---
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
@@ -0,0 +1,135 @@
+package com.kodilla.rps;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RpsGame {
+
+    private String playerName;
+    private boolean end = false;
+    private int noOfRounds;
+    private char playersMove;
+    private char computersMove;
+    private int playersPoints = 0;
+    private int computersPoints = 0;
+    private String availableMoves = "123";
+
+    private Scanner sc = new Scanner(System.in);
+    private Random rnd = new Random();
+
+    public void runGame(){
+        initGame();
+        playRound();
+    }
+
+    public void initGame(){
+        System.out.println("Podaj imię: ");
+        playerName = readPlayersName();
+        System.out.println("Podaj liczbę rund do zagrania: ");
+        noOfRounds = readNoOfRounds();
+        displayWelcomeInformation();
+        displayRules();
+    }
+
+    public String readPlayersName(){
+        return sc.nextLine();
+    }
+
+    public int readNoOfRounds(){
+        return sc.nextInt();
+    }
+
+    public void displayWelcomeInformation(){
+        System.out.println("\nWitaj " + playerName + ". Twoja gra będzie składać się z " + noOfRounds + " rund.\n");
+    }
+
+    public void displayRules(){
+        System.out.println("klawisz 1 - zagranie \"kamień\",\n" +
+                "klawisz 2 - zagranie \"papier\",\n" +
+                "klawisz 3 - zagranie \"nożyce\",\n" +
+                "klawisz x - zakończenie gry,\n" +
+                "klawisz n - uruchomienie gry od nowa,");
+    }
+
+    public void playRound(){
+        displayMoveChoiceRequest();
+        playersMove = readPlayersMove();
+        displayPlayersMove();
+        computersMove = pickComputersMove(availableMoves);
+        displayComputersMove();
+        displayRoundWinnerInfo(playersMove, computersMove);
+        displayScore();
+
+        //policz wynik
+        //pokaz wynik
+        //sprawdz czy kolejna runda
+        //wyswietl zwyciezce
+    }
+
+    public void displayMoveChoiceRequest(){
+        System.out.println("\nWybierz ruch (1 = kamień / 2 = papier / 3 = nożyce) : \n");
+    }
+
+    public char readPlayersMove(){
+        return sc.next().charAt(0);
+    }
+
+    public void displayPlayersMove(){
+        System.out.println("Twoje zagranie to: " + translateMoveIntoDescription(playersMove));
+    }
+
+    public String translateMoveIntoDescription(char move){
+        //change to switch case
+        if(move == '1') return "kamień";
+        if(move == '2') return "papier";
+        if(move == '3') return "nożyce";
+        if(move == 'x') return "koniec gry";
+        if(move == 'n') return "zaczynamy od nowa!";
+        else {
+            System.out.println("Nieporawny ruch");
+            displayMoveChoiceRequest();
+            return "Niepoprawny ruch";
+        }
+    }
+
+    public char pickComputersMove(String availableMoves){
+        return availableMoves.charAt(rnd.nextInt(availableMoves.length()));
+    }
+
+    public void displayComputersMove(){
+        System.out.println("Zagranie komputera to: " + translateMoveIntoDescription(computersMove) + "\n");
+    }
+
+    public void displayWinnerInfo(){
+
+    }
+
+    public void displayRoundWinnerInfo(char playersMove, char computersMove){
+        if(playersMove < computersMove){
+            System.out.println("Wygrałeś rundę!");
+            playersPoints++;
+        }
+        if(computersMove < playersMove){
+            System.out.println("Wygrał komputer!:(");
+            computersPoints++;
+        }
+        if(playersMove == computersMove){
+            System.out.println("Remis!");
+            playersPoints++;
+            computersPoints++;
+        }
+    }
+
+    public void displayScore(){
+        System.out.println("\nObecny Wynik: ");
+        System.out.println(playerName.toUpperCase() + ": " + getPlayersPoints() + " vs COMPUTER: " + getComputersPoints());
+    }
+
+    public int getPlayersPoints() {
+        return playersPoints;
+    }
+
+    public int getComputersPoints() {
+        return computersPoints;
+    }
+}
