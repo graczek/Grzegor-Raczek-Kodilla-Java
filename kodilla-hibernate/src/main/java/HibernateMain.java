ad5097963f796f87646d2a1ca7ed91a0c0f17409
graczek <raczek.grz@gmail.com>
2019-12-19 10:23:40 +0100

    17.1 Hibernate exercise & task start

--- kodilla-hibernate/src/main/java/HibernateMain.java
+++
@@ -1,5 +0,0 @@
-public class HibernateMain {
-    public static void main(String[] args) {
-
-    }
-}
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,13 +1,12 @@
-package com.kodilla.kodilla.spring.web;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaSpringWebApplication {
+public class KodillaHibernateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KodillaSpringWebApplication.class, args);
+        SpringApplication.run(KodillaHibernateApplication.class, args);
     }
-
 }
