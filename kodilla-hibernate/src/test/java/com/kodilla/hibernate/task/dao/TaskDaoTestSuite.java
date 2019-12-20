b7a281a3385798c69c1b6545badc0dd5272b6a9d
graczek <raczek.grz@gmail.com>
2019-12-20 12:02:50 +0100

    17.2 Hibernate exercises

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -49,6 +49,7 @@ public class TaskDaoTestSuite {
         Assert.assertEquals(1, readTasks.size());
 
         //CleanUp
+        //
         int id = readTasks.get(0).getId();
         taskDao.deleteById(id);
     }
